c1ffb11b10fa34f0872d4b641438475817ada82d
Itztiva <tiva@Itztiva.com>
2025-10-18 07:28:53 -0400

    fixed so no more port 3000

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,9 @@ import { cors } from "hono/cors";
 
 const app = new Hono({ strict: false });
 
-export default app;
-
 app.use("*", cors());
 
 app.notFound((c) => c.json(Nexa.basic.notFound, 404));
-
-Bun.serve({
-  port: 5353,
-  fetch: app.fetch,
-});
 
 app.use(async (c, next) => {
   if (c.req.path === "/images/icons/gear.png" || c.req.path === "/favicon.ico") await next();
@@ -30,3 +23,8 @@ app.use(async (c, next) => {
 await loadRoutes(path.join(__dirname, "routes"), app);
 
 logger.backend("Nexa started on port 5353");
+
+export default {
+  port: 5353,
+  fetch: app.fetch,
+}
