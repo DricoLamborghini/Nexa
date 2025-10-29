dc6058082f15893b42058ef943617cc93678d2fe
DricoLamborghini <dricozonemail@gmail.com>
2025-10-29 12:34:25 +0400

    fix bot.ts
    done

--- src/discord/bot.ts
+++ src/discord/bot.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+
 import { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder } from "discord.js";
 import { connectDB } from "../services/mongo";
 import { registerUser } from "../structs/functions";
